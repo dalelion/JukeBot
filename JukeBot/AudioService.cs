eef3bed0fa18ef9c1baeca565dc9037d42740982
D'alelio, Noah <dalelion@wit.edu>
2017-05-29 20:14:44 -0400

    Bot is functioning properly, need to clean up code and work on updating.

---
+++ JukeBot/AudioService.cs
@@ -0,0 +1,81 @@
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Audio;
+using YoutubeExplode;
+using YoutubeExplode.Models;
+using YoutubeExplode.Models.MediaStreams;
+using System.Linq;
+using System;
+using System.Text.RegularExpressions;
+
+public class AudioService {
+
+    public static Task SongPlaying;
+
+    private readonly ConcurrentDictionary<ulong, IAudioClient> ConnectedChannels = new ConcurrentDictionary<ulong, IAudioClient>();
+
+    public async Task JoinAudio (IGuild guild, IVoiceChannel target) {
+        IAudioClient client;
+        if (ConnectedChannels.TryGetValue(guild.Id, out client)) {
+            return;
+        }
+        if (target.Guild.Id != guild.Id) {
+            return;
+        }
+
+        var audioClient = await target.ConnectAsync();
+
+        if (ConnectedChannels.TryAdd(guild.Id, audioClient)) {
+        }
+    }
+
+    public async Task LeaveAudio (IGuild guild) {
+        IAudioClient client;
+        if (ConnectedChannels.TryRemove(guild.Id, out client)) {
+            await client.StopAsync();
+        }
+    }
+
+    public async Task SendAudioAsync (IGuild guild, IMessageChannel channel, string YTLink) {
+
+        YoutubeClient YTC = new YoutubeClient();
+
+        VideoInfo VideoInfo = await YTC.GetVideoInfoAsync(YoutubeClient.ParseVideoId(YTLink));
+
+        AudioStreamInfo ASI = VideoInfo.AudioStreams.OrderBy(x => x.Bitrate).Last();
+
+        String Title = VideoInfo.Title;
+
+        Regex RGX = new Regex("[^a-zA-Z0-9 -]");
+        Title = RGX.Replace(Title, "");
+
+        String Path = "E:/JukeBot/Songs/" + $"{Title}.{ASI.Container.GetFileExtension()}";
+
+        using (var Input = await YTC.GetMediaStreamAsync(ASI))
+        using (var Out = File.Create(Path))
+            await Input.CopyToAsync(Out);
+
+        IAudioClient client;
+
+        if (ConnectedChannels.TryGetValue(guild.Id, out client)) {
+            
+            var Output = CreateStream(Path).StandardOutput.BaseStream;
+            var Stream = client.CreatePCMStream(AudioApplication.Music, 2880);
+            await Output.CopyToAsync(Stream);
+            await Stream.FlushAsync();
+
+        }
+    }
+
+    private Process CreateStream (string path) {
+        return Process.Start(new ProcessStartInfo {
+            FileName = "ffmpeg.exe",
+            Arguments = $"-hide_banner -loglevel panic -i \"{path}\" -ac 2 -f s16le -ar 48000 pipe:1",
+            UseShellExecute = false,
+            RedirectStandardOutput = true
+        });
+    }
+}
--- JukeBot/Program.cs
+++ JukeBot/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
+using Discord.Commands;
 
-namespace JukeBot
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+//MzE4MTQ0MTQzMTE5NjEzOTU0.DAuG2g.AExOqFG35a-OKUM045o5KjDIz-k : JukeBot
+//MzE4NTQ1ODk4MjUyNjY0ODMz.DAz8fw.Xhw6I7pXWa4K2To5lfx1Ebklp_Y : TestingBot
+
+public class Program {
+    // Convert our sync main to an async main.
+    public static void Main (string[] args) =>
+        new Program().Start().GetAwaiter().GetResult();
+
+    private DiscordSocketClient client;
+    private CommandHandler handler;
+
+    public async Task Start () {
+
+        // Define the DiscordSocketClient with a DiscordSocketConfig
+        client = new DiscordSocketClient(new DiscordSocketConfig() { LogLevel = LogSeverity.Info });
+
+        var token = "MzE4MTQ0MTQzMTE5NjEzOTU0.DAuG2g.AExOqFG35a-OKUM045o5KjDIz-k";
+
+        // Login and connect to Discord.
+        await client.LoginAsync(TokenType.Bot, token);
+
+        await client.StartAsync();
+
+        var map = new DependencyMap();
+        map.Add(client);
+
+        handler = new CommandHandler();
+        await handler.Install(map);
+
+        // add logger
+        client.Log += Log;
+
+        // Log the invite URL on client ready
+        client.Ready += Client_Ready;
+
+        // Block this program until it is closed.
+        await Task.Delay(-1);
+    }
+
+    // log the OAuth2 Invite URL of the bot on client ready so that user can see it on startup
+    private async Task Client_Ready () {
+        var application = await client.GetApplicationInfoAsync();
+        await Log(new LogMessage(LogSeverity.Info, "Program",
+            $"Invite URL: <https://discordapp.com/oauth2/authorize?client_id={application.Id}&scope=bot>"));
+    }
+
+    // Bare minimum Logging function for both DiscordSocketClient and CommandService
+    public static Task Log (LogMessage msg) {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
     }
 }
